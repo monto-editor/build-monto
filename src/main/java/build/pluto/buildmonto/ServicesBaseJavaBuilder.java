b5cbbac3bdb870d810f51da0d457482c8c749158
Andre Pacak <andre.pacak@googlemail.com>
2015-09-25 11:06:53 +0200

    format pom

--- src/main/java/build/pluto/buildmonto/ServicesBaseJavaBuilder.java
+++ src/main/java/build/pluto/buildmonto/ServicesBaseJavaBuilder.java
@@ -5,18 +5,16 @@ import build.pluto.builder.Builder;
 import build.pluto.builder.BuilderFactory;
 import build.pluto.builder.BuilderFactoryFactory;
 import build.pluto.buildjava.JavaBuilder;
-import build.pluto.buildjava.compiler.JavacCompiler;
 import build.pluto.buildjava.JavaInput;
 import build.pluto.buildjava.JavaJar;
+import build.pluto.buildjava.JavaJar;
+import build.pluto.buildjava.compiler.JavacCompiler;
 import build.pluto.buildjava.util.FileExtensionFilter;
-
 import build.pluto.buildmaven.MavenDependencyFetcher;
 import build.pluto.buildmaven.input.ArtifactConstraint;
 import build.pluto.buildmaven.input.Dependency;
 import build.pluto.buildmaven.input.MavenInput;
-
 import build.pluto.output.None;
-
 import build.pluto.output.Out;
 import java.io.File;
 import java.io.FileFilter;
@@ -25,12 +23,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
 import org.sugarj.common.FileCommands;
 
 public class ServicesBaseJavaBuilder extends Builder<ServicesBaseJavaInput, None> {
