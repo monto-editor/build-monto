0c047980971c556403a8349c970063f35dffa55a
Sebastian Erdweg <seba.github@gmail.com>
2015-12-03 12:20:01 +0100

    build all

--- src/build/pluto/buildmonto/Main.java
+++ src/build/pluto/buildmonto/Main.java
@@ -1,11 +1,11 @@
 package build.pluto.buildmonto;
 
 import java.io.File;
-
-import org.sugarj.common.Log;
+import java.util.Arrays;
 
 import build.pluto.builder.BuildManagers;
 import build.pluto.builder.BuildRequest;
+import build.pluto.output.None;
 
 public class Main {
 
@@ -16,14 +16,15 @@ public class Main {
       		  new File ("target/services-java"),
       		  new File ("target/services-java.jar"),
       		  null);
-//		BuildManagers.clean(false, new BuildRequest<>(ServicesJava.factory, javaInput));
-        BuildManagers.build(new BuildRequest<>(ServicesJava.factory, javaInput));
         
 		ServicesJavascript.Input jsInput = new ServicesJavascript.Input(
     		  new File ("target/services-javascript"),
     		  new File ("target/services-javascript.jar"),
     		  null);
-      BuildManagers.build(new BuildRequest<>(ServicesJavascript.factory, jsInput));
+		
+		BuildManagers.<None>buildAll(Arrays.asList(
+				new BuildRequest<>(ServicesJava.factory, javaInput), 
+				new BuildRequest<>(ServicesJavascript.factory, jsInput)));
 	}
 
 }
